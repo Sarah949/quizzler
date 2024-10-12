150df97db2fce59a90d7e65628afb63ce73ce3e0
Sarah949 <Sarah-Almahfoudh@hotmail.com>
2024-10-12 21:59:45 +0300

    add question class

---
+++ lib/question.dart
@@ -0,0 +1,7 @@
+class Question {
+  String questionText;
+  bool questionAnswer;
+
+  // Constructor with required named parameters
+  Question({required this.questionText, required this.questionAnswer});
+}
